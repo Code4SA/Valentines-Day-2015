6472891e59a1b6c5d6a6eafac8a47db8d3e847e1
JD Bothma <jbothma@gmail.com>
2016-02-11 10:29:38 +0200

    fix tidied embed

--- times/libs/valentines.js
+++ times/libs/valentines.js
@@ -4,5 +4,5 @@ if (document.location.hostname == "localhost") {
     var baseurl = "http://code4sa.org/Valentines-Day-2016/times/";
 }
 
-document.write('<script type="text/javascript" src="' + baseurl + 'assets/pym.js"></script>')
+document.write('<script type="text/javascript" src="' + baseurl + 'libs/pym.js"></script>')
 document.write(" <script>var pymParent = new pym.Parent('slopeEmbed', '" + baseurl + "index.html', {});</script>")
