a2360aa14aaa88149e6ad920b54a1f7582aa2ed0
Alex Maughan <alex@maughan.me>
2016-02-13 10:04:29 +0200

    Default panels converted to gender specific panels on page load.

--- scorecard/dist/assets/js/jquery.c4saValentines.js
+++ scorecard/dist/assets/js/jquery.c4saValentines.js
@@ -8,12 +8,24 @@ $.c4saValentines = {
 $(document).ready(function () {
 	$.c4saValentines.init();
 	$('html').addClass('js-ready');
-	$('.jsGenderToggle').on('change', function(){
-		var $this = $(this);
+
+	//gender styling
+	var $genderToggles = $('.jsGenderToggle');
+	function toggleGenderStyling($this) {
 		if ($this.attr('value') === 'man') {
 			$this.closest('.panel').removeClass('panel-woman').addClass('panel-man');
 		} else {
 			$this.closest('.panel').removeClass('panel-man').addClass('panel-woman');
 		}
+	}
+	$genderToggles.each(function(i, elem){
+		var $this = $(elem);
+		if ($this.is(':checked')) {
+			toggleGenderStyling($this);
+		}
+	});
+	$genderToggles.on('change', function(){
+		var $this = $(this);
+		toggleGenderStyling($this);
 	});
 });
