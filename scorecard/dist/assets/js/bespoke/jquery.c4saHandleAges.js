d4cb8f8ff809542d13604871d1b9cc46763c3cc8
Adi Eyal <adi@code4sa.org>
2016-02-13 19:54:47 +0200

    Refactored to match Alex's style

--- scorecard/dist/assets/js/bespoke/jquery.c4saHandleAges.js
+++ scorecard/dist/assets/js/bespoke/jquery.c4saHandleAges.js
@@ -19,9 +19,11 @@ $.c4saHandleAges = {
 	imageExtension: '.png',
 	manAge: undefined,
 	womanAge: undefined,
+    youGender: undefined,
 
 	getAges: function() {
 		var youGender = $('input[name=you-gender]:checked').val();
+        $.c4saHandleAges.youGender = youGender;
 		if (youGender === 'man') {
 			$.c4saHandleAges.manAge = $('#jsYouAge').val();
 			$.c4saHandleAges.womanAge = $('#jsPartnerAge').val();
@@ -31,7 +33,7 @@ $.c4saHandleAges = {
 		}
 		$.c4saHandleAges.scoreLoading(true);
 		if ($.c4saHandleAges.dataLoaded) {
-			$.c4saHandleAges.getAgesScore($.c4saHandleAges.manAge, $.c4saHandleAges.womanAge, $.c4saHandleAges.json);
+			$.c4saHandleAges.getAgesScore($.c4saHandleAges.youGender, $.c4saHandleAges.manAge, $.c4saHandleAges.womanAge, $.c4saHandleAges.json);
 		} else {
 			$.c4saHandleAges.loadData();
 		}
@@ -52,73 +54,75 @@ $.c4saHandleAges = {
 				'async': true,
 				'cache': true,
 				'url': $.c4saHandleAges.dataUrl,
-				'dataType': "json",
+				'dataType': 'json',
 				'success': function (data) {
 					$.c4saHandleAges.json = data;
 				}
 			}).done(function() {
 				$.c4saHandleAges.dataLoaded = true;
-				$.c4saHandleAges.getAgesScore($.c4saHandleAges.manAge, $.c4saHandleAges.womanAge, $.c4saHandleAges.json);
+				$.c4saHandleAges.getAgesScore($.c4saHandleAges.youGender, $.c4saHandleAges.manAge, $.c4saHandleAges.womanAge, $.c4saHandleAges.json);
 			});
 		})();
 	},
 
-	getAgesScore: function(manAge, womanAge, data) {
-        var age_ranges = [];
-        for (i = 18; i <= 100; i+=5) {
-            age_ranges.push(i);
+	getAgesScore: function(youGender, manAge, womanAge, data) {
+        youGender = youGender || 'male';
+        var ageRanges = [];
+        for (i = 18; i <= 100; i += 5) {
+            ageRanges.push(i);
         }
 
         var getRangeIndex = function(val) {
-            for (idx in age_ranges) {
-                if (val < age_ranges[idx])
+            for (idx in ageRanges) {
+                if (val < ageRanges[idx])
                     return idx;
             }
-            throw "Value: " + val + " does not fit within range";
+            throw 'Value: ' + val + ' does not fit within range';
         }
-        var marriage_ages = [];
-        for (el in age_ranges) marriage_ages.push(0);
+        var marriageAges = [];
+        for (el in ageRanges) marriageAges.push(0);
 
         var total = 0;
-        var min_age = Infinity;
-        var max_age = -Infinity;
+        var minAge = Infinity;
+        var maxAge = -Infinity;
         for (el in data) {
             var m = data[el];
-            var groom = parseInt(m["groom"]);
-            var bride = parseInt(m["bride"]);
-            var count = parseInt(m["count"]);
+            var groom = parseInt(m['groom']);
+            var bride = parseInt(m['bride']);
+            var count = parseInt(m['count']);
             if (groom == manAge) {
-                min_age = bride < min_age ? bride : min_age;
-                max_age = bride > max_age ? bride : max_age;
+                minAge = bride < minAge ? bride : minAge;
+                maxAge = bride > maxAge ? bride : maxAge;
                 total += count;
                 idx = getRangeIndex(bride);
-                marriage_ages[idx] += count;
+                marriageAges[idx] += count;
             }
         }
-        var ratings = [0, 0.25, 0.5, 0.75];
-        var woman_range = getRangeIndex(womanAge);
-        var num_in_range = marriage_ages[woman_range];
-        var perc = num_in_range / total;
-        var text_ages = String.format("Did you know that a {0} year-old man married a {1} year-old woman, another {0} year-old man married a {2} year-old woman", manAge, min_age, max_age) 
+        var ratings = [0, 0.20, 0.3, 0.40];
+        var womanRange = getRangeIndex(womanAge);
+        var numInRange = marriageAges[womanRange];
+        var perc = numInRange / total;
+        console.log(perc);
+        console.log(marriageAges)
+        var textAges = String.format('Did you know that in that year, a {0} year-old man married a {1} year-old woman, another {0} year-old man married a {2} year-old woman', manAge, minAge, maxAge) 
         if (isNaN(perc)) perc = 0;
 
-        var ratings_text = {
-            0 : "There were no couples of your ages who married in 2014. Even if this isn't illegal you might want to re-consider your choices.",
-            1 : String.format("Only {0} couples of your ages married in 2014. It might work if you are a special couple but it is very unusual.", num_in_range) + " " + text_ages,
-            2 : String.format("Long Shot: Only {0} couples of your ages married in 2014. Not the best odds, but you’re in with a fighting chance. ", num_in_range) + " " + text_ages,
-            3 : "Safe bet!: Definite long-term potential." + " " + text_ages,
-            4 : "Superb!: This match has the strongest love connection." + " " + text_ages
+        var ratingsText = {
+            0 : 'There were no couples of your ages who married in 2014. Even if this isn\'t illegal you might want to re-consider your choices.',
+            1 : String.format('Only {0} couples of your ages married in 2014. It might work if you are a special couple but it is very unusual.', numInRange) + ' ' + textAges,
+            2 : String.format('Long Shot: Only {0} couples of your ages married in 2014. Not the best odds, but you’re in with a fighting chance. ', numInRange) + ' ' + textAges,
+            3 : 'Safe bet!: Definite long-term potential.' + ' ' + textAges,
+            4 : 'Superb!: This match has the strongest love connection.' + ' ' + textAges
         }
 
         var rating = 3;
-        console.log(perc);
         for (idx in ratings) {
             if (perc <= ratings[idx]) {
                 rating = parseInt(idx);
                 break;
             }
         }
-        var text = ratings_text[rating];
+        var text = ratingsText[rating];
         rating -= 1;
         if (rating < 0) rating = 0;
 
