ba7d07785abb51e6a8a71f749b992d9ad227351f
Alex Maughan <alex@maughan.me>
2016-02-13 16:39:18 +0200

    Add loading js to uglify list.

--- scorecard/Gruntfile.js
+++ scorecard/Gruntfile.js
@@ -125,6 +125,7 @@ module.exports = function (grunt) {
 						"dist/assets/js/polyfills/jquery.rangeslider.min.js",
 						"dist/assets/js/modules/jquery.c4saHelpers.js",
 						"dist/assets/js/modules/jquery.c4saForms.js",
+						"dist/assets/js/modules/jquery.c4saLoading.js",
 						"dist/assets/js/bespoke/jquery.c4saHandleAges.js",
 						"dist/assets/js/bespoke/jquery.c4saAgeSlider.js",
 						"dist/assets/js/bespoke/jquery.c4saGenderStyling.js",
