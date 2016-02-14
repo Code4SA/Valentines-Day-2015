0eb55026bbf14a0705615e8b8cc7eedaa7f1a9ab
Adi Eyal <adi@code4sa.org>
2016-02-14 18:41:06 +0200

    Formatted numbers with thousands separators

--- scorecard/dist/assets/js/bespoke/jquery.c4saHandleAges.js
+++ scorecard/dist/assets/js/bespoke/jquery.c4saHandleAges.js
@@ -143,7 +143,7 @@ $.c4saHandleAges = {
         var facts = $.c4saFastFacts;
         var textFacts = '<p><strong>Fast facts:</strong> ' + facts[Math.round(Math.random() * facts.length)] + '</p>';
         var textAges = String.format('Did you know that in that year, a {0} year-old {3} married a {1} year-old {4}, another {0} year-old {3} married a {2} year-old {4}', you.age, minAge, maxAge, you.gender, them.gender);
-        var textContext = String.format('Of the {0} {1} year-old {2} who married in 2014, {3} of them married in the {4} range.', total, you.age, you.gender_plural, numInRange, range);
+        var textContext = String.format('Of the {0} {1} year-old {2} who married in 2014, {3} of them married in the {4} range.', total.toLocaleString(), you.age, you.gender_plural, numInRange.toLocaleString(), range);
 
         if (isNaN(perc)) perc = 0;
 
