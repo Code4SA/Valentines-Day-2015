00e4e58d05a11cc2b974d3b55efc3b37d839ff4a
JD Bothma <jbothma@gmail.com>
2016-07-05 15:12:34 +0200

    fixes

--- scorecard/dist/assets/js/embed.js
+++ scorecard/dist/assets/js/embed.js
@@ -1,8 +1,8 @@
 if (document.location.hostname == "localhost") {
     var baseurl = "";
 } else {
-    var baseurl = "https://static.code4sa.org/Valentines-Day-2016/";
+    var baseurl = "https://static.code4sa.org/Valentines-Day-2016/scorecard/dist/";
 }
 document.write('<div id="code4sa-valentines-index"></div>');
-document.write('<script type="text/javascript" src="' + baseurl + 'scorecard/dist/assets/js/lib/pym.min.js"></script>');
-document.write("<script>var pymParent = new pym.Parent('code4sa-valentines-index', '" + baseurl + "scoecard/dist/index.html', {});</script>");
+document.write('<script type="text/javascript" src="' + baseurl + 'assets/js/lib/pym.min.js"></script>');
+document.write("<script>var pymParent = new pym.Parent('code4sa-valentines-index', '" + baseurl + "index.html', {});</script>");
